cba735c4ead67da6faa7cf31a74c1163c4a5faa5
yosualangkay@student.telkomuniversity.ac.id <yosualangkay@student.telkomuniversity.ac.id>
2022-05-17 13:28:16 +0700

    Membuat halaman login

---
+++ src/components/HalamanLogin.js
@@ -0,0 +1,13 @@
+import React from "react";
+import bootstrap from "bootstrap";
+import '../css/HalamanLogin.css'
+
+function HalamanLogin(){
+    return(
+        <div className="container">
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default HalamanLogin;
