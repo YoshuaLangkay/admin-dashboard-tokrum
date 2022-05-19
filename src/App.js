936deec18e9c5e39ca66f5fb284df57fe1ffb7ce
yosualangkay@student.telkomuniversity.ac.id <yosualangkay@student.telkomuniversity.ac.id>
2022-05-19 14:38:32 +0700

    Membuat notifikasi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HalamanLogin from './components/HalamanLogin';
+import Notifikasi from './components/Notifikasi';
 
 function App() {
   return <>
-    <HalamanLogin />
+    {/* <HalamanLogin /> */}
+    <Notifikasi />
+
   </>
 }
 
