cd24524ca5a66f9888301d053479ad8318d8a384
yosualangkay@student.telkomuniversity.ac.id <yosualangkay@student.telkomuniversity.ac.id>
2022-05-25 12:16:31 +0700

    verifikasi seller

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import HalamanLogin from './components/HalamanLogin';
 import Notifikasi from './components/Notifikasi';
+import VerifikasiSeller from './components/VerifikasiSeller';
 
 function App() {
   return <>
     {/* <HalamanLogin /> */}
-    <Notifikasi />
+    {/* <Notifikasi /> */}
+    <VerifikasiSeller/>
 
   </>
 }
