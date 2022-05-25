468074cc9734b5e0bff3251ee1dd69b1c866d952
dickymaulanaa <dickymaulanaa@gmail.com>
2022-05-25 12:03:18 +0700

    update button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import SideNav from './components/SideNav';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import HalamanLogin from './components/HalamanLogin';
+import SideNav from "./components/SideNav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HalamanLogin from "./components/HalamanLogin";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HalamanLogin/>}/>
-        <Route path="/dasboard" element={<SideNav/>}/>
-      </Routes>
-    </BrowserRouter>
-    
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HalamanLogin />} />
+				<Route path="/dasboard" element={<SideNav />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
